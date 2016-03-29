7d2b2f2cbb54ddbdbc8ad080d9495f277e1da4bd
Patrick Steele-Idem <pnidem@gmail.com>
2016-03-29 09:49:17 -0600

    Search paths should always end with a forward slash

--- transport/src/code-installed.js
+++ transport/src/code-installed.js
@@ -1,6 +1,10 @@
 function installedCode(logicalParentPath, childName, childVersion, options) {
     var modulesRuntimeGlobal = (options && options.modulesRuntimeGlobal) || '$rmod';
 
+    if (logicalParentPath === '/') {
+        logicalParentPath = '';
+    }
+
     var code = modulesRuntimeGlobal + '.installed(' + JSON.stringify(logicalParentPath) + ', ' +
         JSON.stringify(childName) + ', ' +
         JSON.stringify(childVersion);
