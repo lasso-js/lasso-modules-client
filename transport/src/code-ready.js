e37a6c55e4c0428aec20e82ec95a48c14a84d634
Scott Davis <scodavis@ebay.com>
2018-11-27 12:45:59 -0500

    flip logic

--- transport/src/code-ready.js
+++ transport/src/code-ready.js
@@ -1,6 +1,6 @@
 function readyCode(options) {
     var modulesRuntimeGlobal = (options && options.modulesRuntimeGlobal) || '$_mod';
-    return 'if (document.readyState !== "loading") { ' + modulesRuntimeGlobal + '.ready();' + ' } else { document.addEventListener("DOMContentLoaded", ' + modulesRuntimeGlobal + '.ready' + '); }';
+    return 'if (document.readyState === "loading") { document.addEventListener("DOMContentLoaded", ' + modulesRuntimeGlobal + '.ready' + '); } else { ' + modulesRuntimeGlobal + '.ready(); }';
 }
 
 module.exports = readyCode;
