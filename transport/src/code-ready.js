5948e02633cbe8f0e809e790d8076cb5c7f21c98
Scott Davis <scodavis@ebay.com>
2018-11-27 12:36:48 -0500

    wrap ready call with document loaded listener

--- transport/src/code-ready.js
+++ transport/src/code-ready.js
@@ -1,6 +1,6 @@
 function readyCode(options) {
     var modulesRuntimeGlobal = (options && options.modulesRuntimeGlobal) || '$_mod';
-    return modulesRuntimeGlobal + '.ready();';
+    return 'if (document.readyState !== "loading") { ' + modulesRuntimeGlobal + '.ready();' + ' } else { document.addEventListener("DOMContentLoaded", ' + modulesRuntimeGlobal + '.ready' + '); }';
 }
 
 module.exports = readyCode;
